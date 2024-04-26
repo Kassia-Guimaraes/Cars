f993fbb047b76b8c85ada28169b61f3f1def8643
Sara Marques <a85571@alunos.uminho.pt>
2024-04-26 12:41:06 +0100

    Preço-carros

--- testesara.py
+++ testesara.py
@@ -1,6 +1,42 @@
 import pandas as pd
-fuels_price = pd.read_csv('./modificated-data/fuels-price.csv', sep=',')
-electricity_price = pd.read_csv(
-    './initial-data/electricity-price.csv', sep=',')
 
-fuels_price['Electricity (euro/kWh)'] = electricity_price['Eletricidade (euro/kWh)']
+eletrics = pd.read_csv(
+    './modificated-data/consumption-eletrics.csv', sep=',')
+fossilfuels = pd.read_csv(
+    './modificated-data/consumption-fossilfuels.csv', sep=',')
+hydrids = pd.read_csv(
+    './modificated-data/consumption-hydrids.csv', sep=',')
+price_eletrics = pd.read_csv(
+    './initial-data/cars-price/price-eletrics.csv', sep=',')
+price_fossil_fuel = pd.read_csv(
+    './initial-data/cars-price/price-fuelfossil.csv', sep=',')
+price_hybrids = pd.read_csv(
+    './initial-data/cars-price/price-hybrids.csv', sep=',')
+
+consumptions = [eletrics, fossilfuels, hydrids]
+price_fuels = [price_eletrics, price_fossil_fuel, price_hybrids]
+
+for comsumption, price_fuel in zip(consumptions, price_fuels):
+
+    comsumption['Price (euros)'] = [0] * len(comsumption)
+
+    years = price_fuel['Model.year'].values
+    models = price_fuel['Model'].values
+    prices = price_fuel['Price (euros)'].values
+
+    for year, model, price in zip(years, models, prices):
+        condition = (comsumption['Year'] == year) & (
+            comsumption['Model'] == model)
+        comsumption.loc[condition, 'Price (euros)'] = price
+
+    if comsumption.equals(consumptions[0]):
+        comsumption.to_csv(
+            './modificated-data/consumption-eletrics.csv', index=False)
+
+    elif comsumption.equals(consumptions[1]):
+        comsumption.to_csv(
+            './modificated-data/consumption-fossilfuels.csv', index=False)
+
+    elif comsumption.equals(consumptions[2]):
+        comsumption.to_csv(
+            './modificated-data/consumption-hydrids.csv', index=False)
