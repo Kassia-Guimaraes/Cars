914748e56f0363a82a2be1a038debfe20d5c5668
Kassia-Guimaraes <kassiavivereduarda@gmail.com>
2024-04-28 23:49:52 +0100

    add preco dos carros

--- data.py
+++ data.py
@@ -124,7 +124,7 @@ fossil_fuels.to_csv('./modificated-data/consumption-fossilfuels.csv', index=Fals
 
 ### prices 
 
-price_eletrics = pd.read_csv('./initial-data/cars-price/price-eletrics.csv', sep=',')
+price_eletrics = pd.read_csv('./initial-data/cars-price/price-electrics.csv', sep=',')
 price_fossil_fuel = pd.read_csv('./initial-data/cars-price/price-fuelfossil.csv', sep=',')
 price_hybrids = pd.read_csv('./initial-data/cars-price/price-hybrids.csv', sep=',')
 
@@ -134,25 +134,38 @@ price_fuels = [price_eletrics, price_fossil_fuel, price_hybrids]
 
 for comsumption, price_fuel in zip(consumptions, price_fuels):
 
-    comsumption['Price (euros)'] = [0] * len(comsumption)
+    if comsumption.equals(consumptions[1]):
+        comsumption['Price (euros)'] = [0] * len(comsumption)
 
-    years = price_fuel['Model.year'].values
-    models = price_fuel['Model'].values
-    prices = price_fuel['Price (euros)'].values
+        years = price_fuel['Model.year'].values
+        models = price_fuel['Model'].values
+        prices = price_fuel['Price (euros)'].values
+        fuels = price_fuel['Fuel type'].values
 
-    for year, model, price in zip(years, models, prices):
-        condition = (comsumption['Year'] == year) & (
-            comsumption['Model'] == model)
-        comsumption.loc[condition, 'Price (euros)'] = price
+        for year, model, price, fuel in zip(years, models, prices, fuels):
+            condition = (comsumption['Year'] == year) & (
+                comsumption['Model'] == model) & (comsumption['Fuel type'] == fuel)
+            comsumption.loc[condition, 'Price (euros)'] = price
 
-    if comsumption.equals(consumptions[0]):
-        comsumption.to_csv('./modificated-data/consumption-eletrics.csv', index=False)
-
-    elif comsumption.equals(consumptions[1]):
         comsumption.to_csv('./modificated-data/consumption-fossilfuels.csv', index=False)
 
-    elif comsumption.equals(consumptions[2]):
-        comsumption.to_csv('./modificated-data/consumption-hybrids.csv', index=False)
+    else:
+        comsumption['Price (euros)'] = [0] * len(comsumption)
+
+        years = price_fuel['Model.year'].values
+        models = price_fuel['Model'].values
+        prices = price_fuel['Price (euros)'].values
+
+        for year, model, price in zip(years, models, prices):
+            condition = (comsumption['Year'] == year) & (
+                comsumption['Model'] == model)
+            comsumption.loc[condition, 'Price (euros)'] = price
+
+        if comsumption.equals(consumptions[0]):
+            comsumption.to_csv('./modificated-data/consumption-eletrics.csv', index=False)
+
+        elif comsumption.equals(consumptions[2]):
+            comsumption.to_csv('./modificated-data/consumption-hybrids.csv', index=False)
 
 
 
@@ -168,7 +181,7 @@ to_drop = ['Country','VFN','Mp','Mh','Man','MMS','Tan','Va','Ct','Cr','r', 'm (k
            'At2 (mm)','Fm','z (Wh/km)','Electric range (km)','IT','Ernedc (g/km)','Erwltp (g/km)','De','Vf',
            'Date of registration','Fuel consumption ','Status','T']
 
-to_rename = {'V':'Version','Mk':'Make','Cn':'Model','Mt':'Test weight (kg)','Ewltp (g/km)':'Test Emission CO2 (g/km)',
+to_rename = {'Ve':'Version','Mk':'Make','Cn':'Model','Mt':'Test weight (kg)','Ewltp (g/km)':'Test Emission CO2 (g/km)',
              'W (mm)':'Whell Base (mm)','ec (cm3)':'Engine Capacity (cm3)','ep (KW)':'Engine Power (kW)','year':'Year','Ft':'Fuel type'}
 
 
