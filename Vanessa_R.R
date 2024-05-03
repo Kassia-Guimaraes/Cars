3527058681c92dfe2f3346a9107da1a78c9e982c
Vanessa Gomes <vanessa.gomes189@gmail.com>
2024-05-03 18:33:14 +0100

    testes 02/05/2024

--- Vanessa_R.R
+++ Vanessa_R.R
@@ -1,7 +1,9 @@
 library(readr)
 library(zoo)
-PT_2022 <- read_csv("C:/Users/vanes/OneDrive/Desktop/LE/aulas.py/CO2_ProjectShared/modificated-data/PT-2022.csv")
-
+df_all <- read_csv("C:/Users/vanes/OneDrive/Desktop/LE/aulas.py/CO2_ProjectShared/modificated-data/PT-all.csv")
+View(df_all)
+colnames(df_all)
+PT_2022 <- df_all[df_all$Year == 2022, ]
 # Colunas de v.a's quantitativas
 col_numericas <- PT_2022[,c("Test weight (kg)", "Test Emission CO2 (g/km)", "Whell Base (mm)", "Engine Capacity (cm3)", "Engine Power (kW)")]
 
@@ -9,12 +11,13 @@ col_numericas <- PT_2022[,c("Test weight (kg)", "Test Emission CO2 (g/km)", "Whe
 summary_stats <- summary(col_numericas)
 
 # Desvio Padrão das v.a's quantitativas
-desvio_padrao <- round(sapply(col_numericas, sd, na.rm = TRUE),2)
+desvio_padrao <- round(sapply(col_numericas, sd, na.rm = TRUE), 2)
 
 # Summary + sd das v.a's quantitativas
 summary_stats_with_sd <- rbind(summary_stats, "Desvio Padrão" = desvio_padrao)
 View(summary_stats_with_sd)
-
+dados_2022 <- data.frame(summary_stats_with_sd)
+write.csv(dados_2022, "dados_2022_sd_resumoEstat.csv")
 
 # Colunas de v.a's qualitativas
 col_categoricas <- PT_2022[,c("ID", "Version", "Make", "Model", "Fuel type")]
